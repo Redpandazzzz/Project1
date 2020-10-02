cbd1a9e7de2c67c12e832f9a033607100d6627e7
Redpandazzzz <71143680+Redpandazzzz@users.noreply.github.com>
2020-10-01 22:07:58 -0700

    Add files via upload

---
+++ turnin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc > 2) return 1;
+    string strings;
+    string* arr;
+    while (getline(cin, strings)) {
+        if (strings.empty()) { cout << endl; continue; }
+    }
+
+}
+        
